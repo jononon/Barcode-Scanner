0a5a9bd59c7fe6d68d1ec1f9985f560dcd7962c0
Jonathan Damico <jj_damico@yahoo.com>
2015-04-14 21:22:06 -0700

    Full list and accesses

--- src/Scan.java
+++ src/Scan.java
@@ -2,20 +2,28 @@ import java.util.*;
 
 public class Scan {
 	public static void main (String args[]) {
-		String[] reference = new String[100];
+		String[][] reference = new String[100][2];
 		int referenceLength = 0;
-		reference[referenceLength++]="Jonathan Damico";
-		reference[referenceLength++]="DJ Lesh";
+		reference[referenceLength][0]="Jonathan Damico"; 		reference[referenceLength++][1]="All Access";
+		reference[referenceLength][0]="DJ Lesh"; 				reference[referenceLength++][1]="All Access";
+		reference[referenceLength][0]="Jonathan Goldberg"; 		reference[referenceLength++][1]="All Access";
+		reference[referenceLength][0]="Caroline Cook"; 			reference[referenceLength++][1]="Stage";
+		reference[referenceLength][0]="Chase Garvey-Daniels"; 	reference[referenceLength++][1]="Stage";
+		reference[referenceLength][0]="Grace Burton"; 			reference[referenceLength++][1]="Stage";
+		reference[referenceLength][0]="Jaya Nayar"; 			reference[referenceLength++][1]="Stage";
+		reference[referenceLength][0]="Davis Cook"; 			reference[referenceLength++][1]="Stage";
+		reference[referenceLength][0]="Pavan Tauh"; 			reference[referenceLength++][1]="Stage";
+		reference[referenceLength][0]="Princie Kim"; 			reference[referenceLength++][1]="Stage";
 		for(int i = reference.length-1; i>=0; i--) {
-			if(reference[i]==null){}
+			if(reference[i][0]==null){}
 			else
-				System.out.println(reference[i]);
+				System.out.println(reference[i][1]+"---"+reference[i][0]);
 		}
-		System.out.println("Instructions: type 'add' to add names");
+		System.out.println("\nInstructions: type 'add' to add names");
 		Scanner s = new Scanner(System.in);
 		String input;
 		while(true){
-			System.out.println("Ready");
+			System.out.println("\nReady");
 			input = s.nextLine();
 			if(input.equalsIgnoreCase("add")||input.equalsIgnoreCase("Add")){
 				boolean okay = true;
@@ -26,22 +34,25 @@ public class Scan {
 						okay = false;
 						System.out.println("Exited Add Mode");
 					}
-					else
-						reference[referenceLength++]=input;
-				}
-			} else {
-				boolean found = false;
-				for(int i = reference.length-1;i>=0;i--){
-					if(reference[i]==null){}
-					else{
-						if(reference[i].equals(input))
-							found = true;
+					else {
+						reference[referenceLength][0]=input;
+						System.out.println("Access?");
+						reference[referenceLength][0]=s.nextLine();
 					}
 				}
-				if(found)
-					System.out.println("Access Granted");
+			} else {
+				int found = 150;
+				for(int i = reference.length-1;i>=0;i--){
+					if(reference[i][0]==null){}
+					else{
+						if(reference[i][0].equals(input))
+							found = i;
+					}
+				}
+				if(found!=150)
+					System.out.println("Access: "+reference[found][1]);
 				else
-					System.out.println("Access Denied");
+					System.out.println("Access Denied - Lookup Failed");
 			}
 		}
 	}
